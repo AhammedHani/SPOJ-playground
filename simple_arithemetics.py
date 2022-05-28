26e59d60cbf34250191f3d7a6711b25d5888d184
Ahammed Hani <haniahammed7@gmail.com>
2022-05-28 10:04:12 +0530

    simple arithemetics

---
+++ simple_arithemetics.py
@@ -0,0 +1,33 @@
+n = int(input())
+for x in range(n):
+    n1 = int(input())
+    n2 = int(input())
+    n3 = n1 + n2
+    print(" ", n1)
+    print("+", n2)
+    print("-" * 6)
+    print(" ", n3)
+
+    n1 = int(input())
+    n2 = int(input())
+    n3 = n1 - n2
+    print(" ", n1)
+    print("-", n2)
+    print("-" * 6)
+    print(" ", n3)
+
+    n1 = int(input())
+    n2 = int(input())
+    n3 = n1 * n2
+    print(" ", n1)
+    print("*", n2)
+    print("-" * 6)
+    print(" ", n3)
+
+    n1 = int(input())
+    n2 = int(input())
+    n3 = n1 / n2
+    print(" ", n1)
+    print("/", n2)
+    print("-" * 6)
+    print(" ", n3)
